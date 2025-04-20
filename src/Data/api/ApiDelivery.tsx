8ccfaa72fb7106c89eb953e6644896d1d03ec610
Shiuu28 <shiuuvp28@outlook.com>
2025-04-20 15:51:28 -0500

    Feat: update readme.md

--- src/Data/api/ApiDelivery.tsx
+++ src/Data/api/ApiDelivery.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiDelivery = axios.create({
-    baseURL: 'http://10.0.2.2:3001/api/users',
+    baseURL: 'http://10.0.2.2:3000/api/users',
     headers:{
         'Content-Type': 'application/json'
     }
